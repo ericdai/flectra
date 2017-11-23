37f78c619838919a151d337b6b158d7c8d210b8d
ahmed_girach <ahmed@techreceptives.com>
2017-11-23 15:40:38 +0530

    [ADD] Lead Scoring module

---
+++ addons/crm_scoring/__manifest__.py
@@ -0,0 +1,30 @@
+# Part of Flectra. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "CRM Lead Scoring",
+    'category': "Sales",
+    'version': "1.0",
+    'author': 'Flectra',
+    'website': 'https://flectrahq.com',
+    'depends': ['website_crm'],
+    'description': """
+CRM Lead Scoring
+=====================
+
+This module allows you to...
+        Determine the worthiness of leads,
+        by attaching values to them based on their behaviour
+        relating to their interest in your business.
+    """,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/view_crm_lead_score.xml',
+        'views/view_config_crm_contacts.xml',
+        'views/inherit_res_partner_view.xml',
+        'views/crm_lead_view.xml',
+    ],
+    'demo': [
+        'demo/contacts_scoring_demo.xml',
+    ],
+    'installable': True,
+}
